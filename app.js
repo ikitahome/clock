77fcb39c051901c35b5fadd6733861709a77220d
ikita <sitnalta@gmail.com>
2018-12-03 12:33:57 +0800

    Disable analytics for testing

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // --------------------------------
 const settings = {
 	port: 8080,
-	path: "/testing"
+	path: "/testing",
+	path2: "/deploy"
 };
 // --------------------------------
 
@@ -10,8 +11,9 @@ const TimeInImage = require(__dirname+"/time-in-image");
 
 var app = express();
 var ua = require('universal-analytics');
-var timeInImage = new TimeInImage(app, settings.path);
-
+var timeInImage = new TimeInImage(app, settings.path2);
+// No Tracking
+var timeInImage2 = new TimeInImage(app, settings.path);
 
 timeInImage.onRequest = req=>{
 	//let ip = req.ip.split(":")[3];
@@ -30,6 +32,25 @@ timeInImage.onRequest = req=>{
 	visitor.pageview({dp: "/", dt: "Toga & ikita Avatar", dh: "ikitaclock.herokuapp.com/testing", uip: ipAddr}).send();
 	console.log(ipAddr+=" requested the time");
 	console.log("Sent analytics")
+}
+
+timeInImage2.onRequest = req=>{
+	//let ip = req.ip.split(":")[3];
+	
+	
+	let ipAddr = req.headers["x-forwarded-for"];
+	if (ipAddr){
+		var list = ipAddr.split(",");
+		ipAddr = list[list.length-1];
+	} else {
+		ipAddr = req.connection.remoteAddress;
+	}
+	
+	
+	// var visitor = ua('UA-128365036-2');
+	// visitor.pageview({dp: "/", dt: "Toga & ikita Avatar", dh: "ikitaclock.herokuapp.com/testing", uip: ipAddr}).send();
+	// console.log(ipAddr+=" requested the time");
+	// console.log("Sent analytics")
 }
 
 app.listen((process.env.PORT || settings.port), ()=>{
